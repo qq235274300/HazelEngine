4b182cac1e458c1b07a2c7dfae7233c75e924506
Chili <1024747899@qq.com>
2021-11-08 22:29:45 +0800

    Add spdlog ,add Log Class

--- HazelEngine/src/Hazel/EntryPoint.h
+++ HazelEngine/src/Hazel/EntryPoint.h
@@ -8,13 +8,14 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	printf("Engine Start");
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Engine!");
+	int a = 10;
+	HZ_INFO("Hello! Var = {0}",a);
+
 	auto SanBoxApp = Hazel::CreateApplication();
 	SanBoxApp->Run();
 	delete SanBoxApp;
 }
-
-
-
 
 #endif // HAZEL_PLATFORM_WI
